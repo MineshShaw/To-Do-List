a3fe1bb66e36c4e335ad481a9199461b5ba026df
MRGAMING-666709 <mineshshaw2007@gmail.com>
2024-12-26 14:17:52 +0530

    added basic action bar

---
+++ script.js
@@ -0,0 +1,9 @@
+// clock
+setInterval(() => {
+    const date = new Date();
+    const hours = (date.getHours() < 10)? '0' + date.getHours() : date.getHours();
+    const minutes = (date.getMinutes() < 10)? '0' + date.getMinutes() : date.getMinutes();
+    const seconds = (date.getSeconds() < 10)? '0' + date.getSeconds() : date.getSeconds();
+
+    document.getElementById("clock").innerHTML = `${hours}:${minutes}:${seconds}`;
+}, 1000);
